2f05ad03148176bdb2174dbbf1d62c3220fb49fc
Mastoropoulos-Loukas <loukasmastoropoulos@gmail.com>
2022-10-30 18:04:56 +0200

    COMMIT

---
+++ incr.h
@@ -0,0 +1,24 @@
+
+#ifndef INCREMENTAL
+#define INCREMENTAL
+
+
+#include "PolygonGenerator.h"
+#include "shared.h"
+#include"Pick.h"
+
+#include "PolygonGenerator.h"
+
+
+
+
+class IncAlgo : public PolygonGenerator{
+private:
+ArgFlags argFlags;
+public:
+    IncAlgo(PointList&);
+    virtual Polygon_2 generatePolygon(ArgFlags);
+};
+
+
+#endif
